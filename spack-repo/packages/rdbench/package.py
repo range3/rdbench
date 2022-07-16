967da9b9d700baffda529a4a18e316d30a676b9f
range3 <white.line.oar@gmail.com>
2022-07-16 07:17:58 +0000

    Update spack package: add rdbench@0.1.1

--- spack-repo/packages/rdbench/package.py
+++ spack-repo/packages/rdbench/package.py
@@ -16,6 +16,7 @@ class Rdbench(CMakePackage):
     maintainers = ['range3']
 
     version('master', branch='master')
+    version('0.1.1', sha256='ea74c1b96b660352814038b779c361792a3ae068461db84a48dc8d11b01edff1', preferred=True)
     version('0.1.0-1', sha256='5239c1702df9dbdca99cef50966e8624d310f7641b8a8405571cadb948e2de15')
 
     depends_on('mpi')
