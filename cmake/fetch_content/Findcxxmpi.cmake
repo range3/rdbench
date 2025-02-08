d99a5295e5045c7e1f10a506b5a33952dc0bae6b
range3 <white.line.oar@gmail.com>
2025-02-08 17:31:11 +0900

    fix: ensure compatibility with all CMake versions for FetchContent

--- cmake/fetch_content/Findcxxmpi.cmake
+++ cmake/fetch_content/Findcxxmpi.cmake
@@ -1,16 +1,26 @@
-cmake_policy(SET CMP0169 OLD)
 include(FetchContent)
-FetchContent_Declare(
-    cxxmpi 
-    GIT_REPOSITORY https://github.com/range3/cxxmpi.git
-    GIT_TAG master
-    GIT_REMOTE_UPDATE_STRATEGY REBASE
-)
 
-FetchContent_GetProperties(cxxmpi)
-if(NOT cxxmpi_POPULATED)
-  FetchContent_Populate(cxxmpi)
-  add_subdirectory("${cxxmpi_SOURCE_DIR}" "${cxxmpi_BINARY_DIR}" EXCLUDE_FROM_ALL)
+if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.28")
+    FetchContent_Declare(
+        cxxmpi
+        GIT_REPOSITORY https://github.com/range3/cxxmpi.git
+        GIT_TAG master
+        GIT_REMOTE_UPDATE_STRATEGY REBASE
+        EXCLUDE_FROM_ALL
+    )
+    FetchContent_MakeAvailable(cxxmpi)
+else()
+    FetchContent_Declare(
+        cxxmpi
+        GIT_REPOSITORY https://github.com/range3/cxxmpi.git
+        GIT_TAG master
+        GIT_REMOTE_UPDATE_STRATEGY REBASE
+    )
+    FetchContent_GetProperties(cxxmpi)
+    if(NOT cxxmpi_POPULATED)
+        FetchContent_Populate(cxxmpi)
+        add_subdirectory("${cxxmpi_SOURCE_DIR}" "${cxxmpi_BINARY_DIR}" EXCLUDE_FROM_ALL)
+    endif()
 endif()
 
 set(cxxmpi_FOUND 1)
